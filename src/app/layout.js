50a41a869934e5155710e08ebfc08e8c57ca589f
Y.M. Cheung <y.m.cheung@YMs-MacBook-Pro.local>
2023-08-14 10:56:41 +0200

    First commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
-import './globals.css'
+// import './globals.css'
+import 'bootstrap/dist/css/bootstrap.css';
 import { Inter } from 'next/font/google'
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body suppressHydrationWarning={true} className={inter.className}>{children}</body>
     </html>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,154 @@
+"use client"
 import Image from 'next/image'
 import styles from './page.module.css'
 
+import React, {useEffect, useState} from 'react'
+
+// import TodoList from '@/components/Todolist'
+
 export default function Home() {
+  const tasks = [{
+    id: 1,
+    task: 'Test 1',
+    completed: false
+  },{
+    id: 2,
+    task: 'Test 2',
+    completed: false
+  }, {
+    id: 3,
+    task: 'Test 3',
+    completed: true
+  }]
+
+  const [todos, setTodos] = useState(tasks)
+  const [inputTask, setInputTask] = useState('')
+
+  const handleChange = (event) => {
+    setInputTask(event.target.value);
+  } 
+
+  const addTask = (event) => {
+    event.preventDefault()
+    setInputTask('')
+    setTodos((prev) => [
+    ...prev,
+    {
+      id: todos.length + 1,
+      task: inputTask,
+      completed: false,
+    },
+  ]);
+  }
+
+  const deleteTask = (id) => {
+    const removeTaskFromList = todos.filter(item => item.id !== id);
+    const updatedList = updatedTaskIDs(removeTaskFromList)
+    setTodos(updatedList)
+  }
+
+  const completeTask = (id) => {
+   const todoRecordPrevious = todos.find(function (record) {
+      return record.id === id;
+    });
+
+    const todoRecordUpdated = {
+      ...todoRecordPrevious,
+      completed: !todoRecordPrevious.completed,
+    };
+
+    const todosDataNew = todos.map(function (record) {
+      return record.id === id ? todoRecordUpdated : record;
+    });
+
+    setTodos(todosDataNew);
+  }
+
+  const updatedTaskIDs = (todosList) => {
+    for (let index = 0; index < todosList.length; index++) {
+      todosList[index].id = index +1;
+    }
+    return todosList
+  }
+  
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <section className="vh-100">
+        <div className="container py-5 h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col col-lg-9 col-xl-7">
+              <div className="card rounded-3">
+                <div className="card-body p-4">
+
+                  <h4 className="text-center my-3 pb-3">To Do App</h4>
+
+                  <form className="row row-cols-lg-auto g-3 justify-content-center align-items-center mb-4 pb-2">
+                    <div className="col-12">
+                      <div className="">
+                        <input 
+                          type="text"
+                          id="form1" 
+                          className="form-control"
+                           placeholder='Enter a task here' 
+                           value={inputTask}
+                           onChange={handleChange} />
+                      </div>
+                    </div>
+
+                    <div className="col-12">
+                      <button 
+                        type="submit" 
+                        className="btn btn-primary" 
+                        onClick={addTask} 
+                        disabled={inputTask.length === 0 ? true : false}
+                      >Save</button>
+                    </div>
+                  </form>
+
+                  <table className="table mb-4">
+                    <thead>
+                      <tr>
+                        <th scope="col">No.</th>
+                        <th scope="col">Task</th>
+                        <th scope="col">Actions</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {
+                        todos.map(todo => 
+                          <tr key={todo.id}>
+                            <th scope="row">{todo.id}</th>
+                            <td>{todo.task}</td>
+                            <td>
+                             
+                              <button 
+                                type="submit" 
+                                className="btn btn-danger" 
+                                disabled={todo.completed !== false ? false : true}
+                                onClick={() => deleteTask(todo.id)}
+                              >
+                                <span className="bi bi-trash"></span>
+                              </button>
+                              <button 
+                                type="submit" 
+                                className="btn btn-success ms-1"  
+                                disabled={todo.completed === false ? false : true}
+                                onClick={() => completeTask(todo.id)}
+                              >
+                                <i className="bi bi-check-circle"></i>
+                              </button>
+                            </td>
+                          </tr>
+                        )
+                      }
+                    </tbody>
+                  </table>
+
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </section>
+ 
   )
 }
